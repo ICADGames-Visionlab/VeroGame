8f5c584e484af0d1014fb9f6352c0635d2ff4bd2
maumau27 <mauriciolana@hotmail.com>
2017-09-10 19:20:01 -0300

    Commit com: -Scripts

---
+++ VeroGame/Assets/Scripts/DataStorage.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataStorage : MonoBehaviour {
+
+    public class Processo
+    {
+        int id;
+        string nome;
+    }
+
+    public class Nivel
+    {
+        int id;
+        string nome;
+    }
+
+    public class Case
+    {
+        int id;
+        string nome;
+    }
+
+    public class Pergunta
+    {
+        int id;
+        string texto;
+        Resposta[] respostas;
+    }
+
+    public class Resposta
+    {
+        int id;
+        string texto;
+    }
+
+    public class UsuarioResposta
+    {
+        int respostaId;
+        int caseId;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ VeroGame/Assets/Scripts/DynamicSceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicSceneController : MonoBehaviour {
+    
+    public struct NamedImage
+    {
+        public string name;
+        public Texture2D image;
+    }
+    public NamedImage pictures;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
